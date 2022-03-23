1b72b217cc325580e5b483dcaa4ffb4e396271dc
kazimirez <kazimirpwnz@gmail.com>
2022-03-23 15:18:40 +0300

    Product page now test 10 pages via mark parametrize

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,10 +4,10 @@ import math
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=5):
+    def __init__(self, browser, url, ):  # timeout=5
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,25 @@
 import time
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
+
+
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
     #link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
+    #link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
     this_page = ProductPage(browser, link)
     this_page.open()
     this_page.press_add_to_cart()
@@ -13,4 +27,5 @@ def test_guest_can_add_product_to_basket(browser):
     this_page.should_be_the_right_book()
     this_page.should_be_the_right_sum()
     time.sleep(5)
+
 
